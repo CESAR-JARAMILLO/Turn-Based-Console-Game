94eb97b5a2ef6141c9e14beb63a6453f917cf695
CESAR-JARAMILLO <31831282+CESAR-JARAMILLO@users.noreply.github.com>
2023-02-04 00:17:26 -0800

    Update Program.cs
    Created game logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,49 @@
-﻿class Character
+Character hero = new Character("Hero", 80, 22, 14, 38);
+Character villain = new Character("Villain", 90, 21, 12, 40);
+
+Console.WriteLine("{0} has challenged you to a fight!", villain.Name);
+Thread.Sleep(2000);
+
+            while (villain.Hp > 0 || hero.Hp > 0)
+            {
+                Console.WriteLine("Type an option from the menu: (attack, special)  ");
+                string menu = Console.ReadLine();
+                int villainSpecial = new Random().Next(0, 2);
+
+                if (menu == "attack")
+                {
+                    hero.Attack(villain);
+                }
+                else if (menu == "special")
+                {
+                    hero.SpecialAttack(villain);
+                }
+
+                if (villain.Hp <= 0)
+                {
+                    Console.WriteLine("{0} has been defeated. Congratulations!", villain.Name);
+                    break;
+                }
+
+                Thread.Sleep(2000);
+
+                if (villainSpecial == 1)
+                {
+                    villain.SpecialAttack(hero);
+                }
+                else
+                {
+                    villain.Attack(hero);
+                }
+
+                if (hero.Hp <= 0)
+                {
+                    Console.WriteLine("{0} has been defeated. Game over!", hero.Name);
+                    break;
+                }
+            }
+
+class Character
 {
     // Character Atributes
     public string? name { get; set; }
@@ -62,7 +107,4 @@
             Console.WriteLine();
         }
     }
-
-    Character hero = new Character("Hero", 80, 22, 14, 38);
-    Character villain = new Character("Villain", 96, 23, 16, 40);
 }
