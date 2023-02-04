b281b804d046db30c474c368a3f4b70f67b4a677
Cesar <csarjaramillo89@yahoo.com>
2023-02-03 23:22:51 -0800

    create Character method

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 ﻿class Character
 {
     // Character Atributes
-    public string? Name { get; set; }
-    public int Hp { get; set; }
-    public int AttackPoints { get; set; }
-    public int DefensePoints { get; set; }
-    public int SpecialPoints { get; set; }
+    public string? name { get; set; }
+    public int hp { get; set; }
+    public int attackPoints { get; set; }
+    public int defensePoints { get; set; }
+    public int specialPoints { get; set; }
+
+    public Character(string name, int attackPoints, int defensePoints, int specialPoints)
+    {
+        this.name = name;
+        this.attackPoints = attackPoints;
+        this.defensePoints = defensePoints;
+        this.specialPoints = specialPoints;
+    }
 }
