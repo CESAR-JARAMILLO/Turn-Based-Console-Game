17db64ef2b661d7679e8c2155f82f3203176f2f3
Cesar <csarjaramillo89@yahoo.com>
2023-02-03 23:41:44 -0800

    created attack method with miss possibility

--- Program.cs
+++ Program.cs
@@ -15,4 +15,27 @@
         this.defensePoints = defensePoints;
         this.specialPoints = specialPoints;
     }
+
+    public void attack(Character opponent)
+    {
+        Random random = new Random();
+        int miss = random.Next(0, 6);
+        if (miss == 0)
+        {
+            Console.WriteLine($"{name}'s attack missed");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        else if (attackPoints > opponent.defensePoints)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($"{name} attacked {opponent.name}.");
+            opponent.hp = opponent.hp + opponent.defensePoints - attackPoints;
+            Console.WriteLine($"{opponent.name} has {opponent.hp} HP left.");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
 }
